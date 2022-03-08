9bf6cbef4f4f40c1b97354a5461773582f7a4cef
Austin Torres <78386688+austintorres578@users.noreply.github.com>
2022-03-08 17:16:39 -0500

    First Upload

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function NavBar(){
+    return (
+        <nav className="home-nav-bar">
+            <div className="nav-logo">
+                <h1>Austin Site</h1>
+            </div>
+            <div className="nav-links">
+                <ul>
+                    <li>Reviews</li>
+                </ul>
+            </div>
+        </nav>
+    )
+};
