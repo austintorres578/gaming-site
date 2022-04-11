94cae263d2529cd83e84c5505577ce16f8403ce9
Austin Torres <78386688+austintorres578@users.noreply.github.com>
2022-04-11 13:24:52 -0400

    site logo/ in progress
    -changed site logo
    -changed title logo.
    -added a background color to desk in progress

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,11 @@
 import React from "react";
-import logo from "../images/site-icon.png"
+import logo from "../images/Screenshot (179).png"
 
 export default function NavBar(){
     return (
         <nav id="home-nav-bar" className="home-nav-bar">
             <div className="nav-logo">
                 <img className="site-logo" src={logo}></img>
-                <h1>CPG Reviews</h1>
             </div>
             <div className="nav-links">
                 <ul>
